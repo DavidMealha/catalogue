1a0807db5f9ea81765570e5b6781c30387174074
David Mealha <davidmealha@hotmail.com>
2018-03-13 16:55:23 +0000

    Update main.go
    Solving compilation errors

--- cmd/cataloguesvc/main.go
+++ cmd/cataloguesvc/main.go
@@ -9,8 +9,8 @@ import (
 	"syscall"
 
 	"github.com/go-kit/kit/log"
-	stdopentracing "github.com/opentracing/opentracing-go"
-	zipkin "github.com/openzipkin/zipkin-go-opentracing"
+	//stdopentracing "github.com/opentracing/opentracing-go"
+	//zipkin "github.com/openzipkin/zipkin-go-opentracing"
 
 	"net"
 	"net/http"
@@ -46,7 +46,7 @@ func main() {
 		port   = flag.String("port", "8081", "Port to bind HTTP listener") // TODO(pb): should be -addr, default ":8081"
 		images = flag.String("images", "./images/", "Image path")
 		dsn    = flag.String("DSN", "catalogue_user:default_password@tcp(catalogue-db:3306)/socksdb", "Data Source Name: [username[:password]@][protocol[(address)]]/dbname")
-		zip    = flag.String("zipkin", os.Getenv("ZIPKIN"), "Zipkin address")
+		//zip    = flag.String("zipkin", os.Getenv("ZIPKIN"), "Zipkin address")
 	)
 	flag.Parse()
 
@@ -66,11 +66,11 @@ func main() {
 	var logger log.Logger
 	{
 		logger = log.NewLogfmtLogger(os.Stderr)
-		logger = log.NewContext(logger).With("ts", log.DefaultTimestampUTC)
-		logger = log.NewContext(logger).With("caller", log.DefaultCaller)
+		logger = log.With("ts", log.DefaultTimestampUTC)
+		logger = log.With("caller", log.DefaultCaller)
 	}
 
-	var tracer stdopentracing.Tracer
+	/*var tracer stdopentracing.Tracer
 	{
 		if *zip == "" {
 			tracer = stdopentracing.NoopTracer{}
@@ -103,7 +103,7 @@ func main() {
 			}
 		}
 		stdopentracing.InitGlobalTracer(tracer)
-	}
+	}*/
 
 	// Data domain.
 	db, err := sqlx.Open("mysql", *dsn)
@@ -127,20 +127,21 @@ func main() {
 	}
 
 	// Endpoint domain.
-	endpoints := catalogue.MakeEndpoints(service, tracer)
+	endpoints := catalogue.MakeEndpoints(service)
 
 	// HTTP router
-	router := catalogue.MakeHTTPHandler(ctx, endpoints, *images, logger, tracer)
+	router := catalogue.MakeHTTPHandler(ctx, endpoints, *images, logger)
 
-	httpMiddleware := []middleware.Interface{
+	/*httpMiddleware := []middleware.Interface{
 		middleware.Instrument{
 			Duration:     HTTPLatency,
 			RouteMatcher: router,
 		},
-	}
+	}*/
 
 	// Handler
-	handler := middleware.Merge(httpMiddleware...).Wrap(router)
+	handler := router
+	//handler := middleware.Merge(httpMiddleware...).Wrap(router)
 
 	// Create and launch the HTTP server.
 	go func() {
